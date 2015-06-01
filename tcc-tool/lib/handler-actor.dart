dceac462614aae2cb6d8b1652ed501ae8dc0d809
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-01 15:04:01 +0200

    Initial commit of server-side tool.

---
+++ tcc-tool/lib/handler-actor.dart
@@ -0,0 +1,47 @@
+part of tcctool.router;
+
+class Actor {
+
+  String _jsonStorePath = 'json';
+
+  Future<shelf.Response> get (shelf.Request request) {
+    String name = shelf_route.getPathParameter(request, 'name');
+
+    IO.File file = new IO.File ('$_jsonStorePath/$name.json');
+
+    return file.readAsString().then((String result) =>
+      new shelf.Response.ok (result));
+  }
+
+  shelf.Response list (shelf.Request request) {
+    List<IO.FileSystemEntity> files = new IO.Directory ('$_jsonStorePath').listSync();
+
+    String result = JSON.encode(files.map((IO.FileSystemEntity f) =>
+      f.path.substring(_jsonStorePath.length+1,f.path.length-5)).toList());
+
+    return new shelf.Response.ok (result);
+  }
+
+  Future<shelf.Response> create (shelf.Request request) {
+    String name = shelf_route.getPathParameter(request, 'name');
+
+    IO.File file = new IO.File ('$_jsonStorePath/$name.json');
+
+    return request.readAsString().then(file.writeAsString).then((_) => new shelf.Response.ok ('ok'));
+  }
+
+  Future<shelf.Response> update (shelf.Request request) {
+    String name = shelf_route.getPathParameter(request, 'name');
+
+    IO.File file = new IO.File ('$_jsonStorePath/$name.json');
+
+    return request.readAsString().then(file.writeAsString).then((_) => new shelf.Response.ok ('ok'));
+  }
+
+  Future<shelf.Response> remove (shelf.Request request) {
+    String name = shelf_route.getPathParameter(request, 'name');
+
+    IO.File file = new IO.File ('$_jsonStorePath/$name.json');
+    return  file.delete().then((_) => new shelf.Response.ok ('ok'));
+  }
+}
---
+++ tcc-tool/lib/router.dart
@@ -0,0 +1,53 @@
+library tcctool.router;
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'dart:io' as IO;
+
+import 'package:logging/logging.dart';
+
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_route/shelf_route.dart' as shelf_route;
+import 'package:shelf_cors/shelf_cors.dart' as shelf_cors;
+
+part 'handler-actor.dart';
+
+const String libraryName = 'tcctool.router';
+final Logger log = new Logger (libraryName);
+
+final Map corsHeaders =
+  {'Access-Control-Allow-Origin': '*' ,
+   'Access-Control-Allow-Methods' : 'GET, PUT, POST, DELETE'};
+
+
+/// Simple access logging.
+void _accessLogger(String msg, bool isError) {
+  if (isError) {
+    log.severe(msg);
+  } else {
+    log.finest(msg);
+  }
+}
+
+Future<IO.HttpServer> start({String hostname : '0.0.0.0', int port : 7777}) {
+  Actor actorHandler = new Actor();
+
+
+  var router = shelf_route.router()
+    ..get('/actor', actorHandler.list)
+    ..get('/actor/{name}', actorHandler.get)
+    ..put('/actor/{name}', actorHandler.update)
+    ..post('/actor/{name}', actorHandler.create)
+    ..delete('/actor/{name}', actorHandler.remove);
+  var handler = const shelf.Pipeline()
+      .addMiddleware(shelf_cors.createCorsHeadersMiddleware(corsHeaders : corsHeaders))
+      .addMiddleware(shelf.logRequests(logger : _accessLogger))
+      .addHandler(router.handler);
+
+  log.fine('Serving interfaces:');
+  shelf_route.printRoutes(router, printer : log.fine);
+
+  return shelf_io.serve(handler, hostname, port);
+}
