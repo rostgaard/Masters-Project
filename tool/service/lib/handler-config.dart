81cec70ea7cbb2b9c791ed3cf197e4210ee4eff4
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-09 11:20:34 +0200

    Use the store and constant and return appropriate values when no file is found.

--- tool/service/lib/handler-config.dart
+++ tool/service/lib/handler-config.dart
@@ -2,10 +2,15 @@ part of tcctool.router;
 
 class Config {
 
-  String _jsonStorePath = '/tmp/json';
+  final String _jsonStorePath = fileStore;
+  final Logger log = new Logger('$libraryName.Config');
 
   Future<shelf.Response> get (shelf.Request request) {
     IO.File file = new IO.File ('$_jsonStorePath/config.json');
+
+    if(!file.existsSync()) {
+      new shelf.Response.notFound('No config found');
+    }
 
     return file.readAsString().then((String result) =>
       new shelf.Response.ok (result));
@@ -13,6 +18,7 @@ class Config {
 
   Future<shelf.Response> update (shelf.Request request) {
     IO.File file = new IO.File ('$_jsonStorePath/config.json');
+    file.createSync();
 
     return request.readAsString().then(file.writeAsString).then((_) => new shelf.Response.ok ('ok'));
   }
