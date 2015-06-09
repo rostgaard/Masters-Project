85ef83e78bbc87aca555c9d78327e009019494f3
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-09 11:18:46 +0200

    Inital constructor and default value.

--- tool/libtcc/lib/src/configuration.dart
+++ tool/libtcc/lib/src/configuration.dart
@@ -3,9 +3,11 @@ part of libtcc.base;
 
 class Configuration {
 
-  Uri jenkinsUri;
+  Uri jenkinsUri = Uri.parse('http://localhost/jenkins');
 
   Configuration();
+
+  factory Configuration.initial() => new Configuration();
 
   Configuration.fromMap(Map map) :
     jenkinsUri = Uri.parse(map[Key.jenkinsUri]);
