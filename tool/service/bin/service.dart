83e4497d342f068d7bd567abcc282073c2c09134
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-09 14:20:11 +0200

    Enabed logging.

--- tool/service/bin/service.dart
+++ tool/service/bin/service.dart
@@ -2,8 +2,12 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import '../lib/router.dart';
+import 'package:logging/logging.dart';
 
 main() {
+
+  Logger.root.level= Level.ALL;
+  Logger.root.onRecord.listen(print);
 
   start();
 }
