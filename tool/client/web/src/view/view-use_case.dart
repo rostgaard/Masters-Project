f4701fa1422e55b17aef38f24ab52b3df67668cd
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-09 15:01:10 +0200

    Pre- and postconditions.

--- tool/client/web/src/view/view-use_case.dart
+++ tool/client/web/src/view/view-use_case.dart
@@ -1,14 +1,33 @@
 part of tcc.client.view;
 
-///TODO: implement.
 class UseCase {
   final HeadingElement _name = new HeadingElement.h1();
   final ParagraphElement _description = new ParagraphElement();
   final UListElement _scenario = new UListElement();
+  final UListElement _preconditions = new UListElement();
+  final UListElement _postconditions = new UListElement();
+
+  final HeadingElement scenarioLabel = new HeadingElement.h2()
+    ..text = 'Scenario';
+
+  final HeadingElement preconditionLabel = new HeadingElement.h2()
+    ..text = 'Preconditions';
+
+  final HeadingElement postconditionLabel = new HeadingElement.h2()
+    ..text = 'Postconditions';
 
   DivElement get element => new DivElement()
     ..classes.add('use-case')
-    ..children = [_name, _description, _scenario];
+    ..children = [
+      _name,
+      _description,
+      scenarioLabel,
+      _scenario,
+      preconditionLabel,
+      _preconditions,
+      postconditionLabel,
+      _postconditions
+    ];
 
   /**
    * Default constructor.
@@ -23,6 +42,30 @@ class UseCase {
     _description.text = uc.description;
     _scenario.children =
         uc.statements.map(_statementToLI).toList(growable: false);
+    _preconditions.children =
+        uc.preconditions.map(_predicateToLI).toList(growable: false);
+    _postconditions.children =
+        uc.postconditions.map(_predicateToLI).toList(growable: false);
+  }
+
+  /**
+   * Convert a predicate into a visual LI element.
+   */
+  LIElement _predicateToLI(libtcc.Predicate pred) {
+    SpanElement expectationElement = new SpanElement()
+      ..text = '${pred.expectation} '
+      ..classes.add('expectation');
+
+    SpanElement exprElement = new SpanElement()
+      ..text = '${pred.expr} '
+      ..classes.add('expression');
+
+    SpanElement matcherElement = new SpanElement()
+      ..text = '${pred.matcher} '
+      ..classes.add('matcher');
+
+    return new LIElement()
+      ..children = [exprElement, matcherElement, expectationElement];
   }
 
   /**
